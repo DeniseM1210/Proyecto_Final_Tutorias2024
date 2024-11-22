f93207b2f53baa0bd954731b27dff223e08f4c24
Cesar1xd1 <cesar1xd1@gmail.com>
2024-11-21 18:35:24 -0600

    Se añadio una imagen en lugar de la palabra logo y agrego la misma en una carpeta

--- backend/pages/menu_principal.php
+++ backend/pages/menu_principal.php
@@ -7,14 +7,21 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <link rel="stylesheet" href="../styles/style.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-sm navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="javascript:void(0)">Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <a href="https://www.tecnm.mx">
+                <img src="../imgs/LOGO_TECNM_BLANCO.png" width="150" height="60">
+            </a>
+            
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar"
+            aria-controls="mynavbar"
+            aria-expanded="false" aria-label="Toggle navigation" >
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="mynavbar">
