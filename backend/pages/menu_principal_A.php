4f68f628174ffe5a327291f74af96c173aacfe77
Denise <denisemorales8764@gmail.com>
2024-12-10 11:23:52 -0600

    Se hicieron cambios en el menu de alumnos

--- backend/pages/menu_principal_A.php
+++ backend/pages/menu_principal_A.php
@@ -23,7 +23,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light ">
         <div class="container-fluid">
-        <a class="navbar-brand" href="menu_principal_T"><i style="font-size: 30px; color: white;" class="bi bi-house-fill" height="500"></i></a>
+        <a class="navbar-brand" href="menu_principal_A"><i style="font-size: 30px; color: white;" class="bi bi-house-fill" height="500"></i></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
